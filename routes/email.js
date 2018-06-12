78084461e61c1677faf630493667ac31d59822b8
DESKTOP-455I8U2\Suporte <dhiego_ti@hotmail.com>
2018-06-12 16:22:22 -0300

    Deletando arquivos desnecessarios

--- routes/email.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const emailController = require('../controllers/email')
-
-/* GET users listing. */
-router.get('/', emailController.montarEmail);
-router.post('/', emailController.enviarEmail);
-
-module.exports = router;
